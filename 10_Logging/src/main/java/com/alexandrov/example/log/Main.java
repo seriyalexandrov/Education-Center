2bd8ca0c6a3fd98287ee804bf3c5b1209a97c227
Ser <seriyalexandrov@mail.ru>
2016-11-08 20:44:54 +0300

    update log example

--- 10_Logging/src/main/java/com/alexandrov/example/log/Main.java
+++ 10_Logging/src/main/java/com/alexandrov/example/log/Main.java
@@ -8,6 +8,37 @@ public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
+
         log.error("Error");
+        log.info("Info");
+        log.debug("Debug");
+        log.trace("Trace");
+
+
+        if (log.isDebugEnabled()) {
+            log.debug("debug enabled", longCalculation());
+        }
+
+        if (log.isTraceEnabled()) {
+            log.trace("debug enabled", longCalculation());
+        }
+
+        POJO pojo = new POJO();
+        pojo.name = "a";
+        pojo.surname = "b";
+        pojo.age = 17;
+
+        log.debug("Log pojo", pojo);
+    }
+
+    private static String longCalculation() {
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return "log";
     }
 }
